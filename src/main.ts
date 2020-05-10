64d58441edde93376a7807cda92f7cac536c18f0
Ruben Aguilera <ruben.aguilera@kairosds.com>
2020-05-10 07:42:26 +0200

    Configure to k8s

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('nest-workshop');
   const options = new DocumentBuilder()
     .setTitle('Nest Workshop')
     .setDescription('Nest Workshop')

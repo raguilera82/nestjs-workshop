bbfee9df0bfcd47388cd9d56bdcf6d5231229cc6
Ruben Aguilera <ruben.aguilera@kairosds.com>
2020-05-10 07:47:18 +0200

    Add global prefix to api swagger

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('nest-workshop/api', app, document);
   await app.listen(3000);
 }
 bootstrap();
